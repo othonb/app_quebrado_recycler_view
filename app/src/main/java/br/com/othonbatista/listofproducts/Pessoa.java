78d74ab6c9b72aeae447ebc9e8900e793d79b0db
Othon Batista <othonbatista@gmail.com>
2022-06-03 20:22:42 -0300

    App consertado e com Lombok integrado

--- app/src/main/java/br/com/othonbatista/listofproducts/Pessoa.java
+++ app/src/main/java/br/com/othonbatista/listofproducts/Pessoa.java
@@ -1,38 +1,14 @@
 package br.com.othonbatista.listofproducts;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
 public class Pessoa {
 
     private String nome;
     private String idade;
     private int fotoId;
 
-    Pessoa(String nome, String idade, int fotoId) {
-        this.nome = nome;
-        this.idade = idade;
-        this.fotoId = fotoId;
-    }
-
-    public String getNome() {
-        return nome;
-    }
-
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
-    public String getIdade() {
-        return idade;
-    }
-
-    public void setIdade(String idade) {
-        this.idade = idade;
-    }
-
-    public int getFotoId() {
-        return fotoId;
-    }
-
-    public void setFotoId(int fotoId) {
-        this.fotoId = fotoId;
-    }
 }
